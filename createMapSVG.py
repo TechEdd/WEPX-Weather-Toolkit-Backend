d2a177d76ee7d329a41e6b3ec80994f3509268bb
Eddie Pelletier <Eddiepelletier.2006@gmail.com>
2024-11-22 18:40:56 -0500

    Revert "better resolution"
    This reverts commit 86d913180d7997f1fd0aa0c339bf652d30e79871.

--- createMapSVG.py
+++ createMapSVG.py
@@ -3,10 +3,13 @@ import cartopy.crs as ccrs
 import cartopy.feature as cfeature
 
 
-extent = [-180,180, -90, 90]
+extent = [-134.12142793280148,
+        -60.92779791187436,
+        21.14706163554821,
+        52.62870288555903]
 
 # Create a figure with a specific size and equirectangular projection
-fig, ax = plt.subplots(figsize=(72, 64), 
+fig, ax = plt.subplots(figsize=(12, 8), 
                        subplot_kw={'projection': ccrs.PlateCarree()})
 
 
@@ -31,10 +34,10 @@ provinc_bodr = cfeature.NaturalEarthFeature(category='cultural',
     name='admin_1_states_provinces_lines', scale='10m', facecolor='none')
 
 
-#ax.add_feature(counties, edgecolor='gray', linewidth=0.1)
-#ax.add_feature(country_bodr, linewidth=0.05, edgecolor="k")  #USA/Canada
-ax.add_feature(provinc_bodr, linewidth=0.1, edgecolor="k")
-ax.add_feature(cfeature.BORDERS, linewidth=1, edgecolor="k")
+ax.add_feature(counties, edgecolor='gray', linewidth=0.1)
+ax.add_feature(country_bodr, linewidth=0.6, edgecolor="k")  #USA/Canada
+ax.add_feature(provinc_bodr, linewidth=0.6, edgecolor="k")
+ax.add_feature(cfeature.BORDERS, linewidth=0.3)
 
 
 # Optionally, add land and ocean features for better visualization
@@ -50,11 +53,10 @@ ax.get_yaxis().set_visible(False)
 plt.subplots_adjust(left=0, right=1, top=1, bottom=0)
 
 # Set the map extent to the desired bounding box
-##ax.set_extent(extent, crs=ccrs.PlateCarree())
+ax.set_extent(extent, crs=ccrs.PlateCarree())
 
 # Save the figure as an SVG without any borders
-plt.savefig("full_map.svg", format='svg', bbox_inches='tight', pad_inches=0, transparent=True)
-plt.savefig("full_map.png", format='png', dpi=300, bbox_inches='tight', pad_inches=0, transparent=True)
+plt.savefig("map_with_counties_fullscreen.svg", format='svg', bbox_inches='tight', pad_inches=0, transparent=True)
 
 # Show the plot (optional)
 plt.show()
